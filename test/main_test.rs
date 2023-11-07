80fc42e8d9a705dc9b7e3e29c7e96473202362ef
b0a04gl <balaji.foo404@gmail.com>
2023-11-07 08:43:46 +0530

    added unit tests #2

--- test/main_test.rs
+++ test/main_test.rs
@@ -21,4 +21,25 @@ mod main_tests {
         assert_eq!(hash, expected_hash);
     }
 
+    #[test]
+    fn test_find_duplicates() {
+        let temp_dir = TempDir::new().expect("Failed to create a temporary directory");
+
+        
+        let content = b"Duplicate content";
+        let mut files: Vec<PathBuf> = Vec::new();
+
+        for _ in 0..3 {
+            let file_path = temp_dir.path().join("duplicate.txt");
+            let mut test_file = File::create(&file_path).expect("Failed to create a test file");
+            test_file.write_all(content).expect("Failed to write to the test file");
+            files.push(file_path);
+        }
+
+        let duplicate_files = find_duplicates(temp_dir.path()).expect("Failed to find duplicates");
+
+        assert_eq!(duplicate_files.len(), 1); 
+        let (_, files) = duplicate_files.iter().next().expect("No duplicates found");
+        assert_eq!(files.len(), 3); 
+    }
 }
